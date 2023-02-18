a8acbb4e69ffaac9701d6af64f9d80dbe21fab3d
Monte <monte6198@gmail.com>
2023-02-19 03:57:03 +0900

    feat: Navbar 스타일 수정

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import Navigation from './shared/Navigation/Navigation.component';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <Navigation />
+      <hr />
       <Routes>
         <Route path='/' element={<div>Muroom Homepage</div>}></Route>
         <Route path='/places' element={<div>Muroom Places Page</div>}></Route>
         <Route path='/map' element={<div>Muroom Map Page</div>}></Route>
         <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/shared/Navigation/Navigation.component.jsx
+++ src/shared/Navigation/Navigation.component.jsx
@@ -4,35 +4,21 @@ import './Navigation.styles.scss';
 
 const Navigation = () => {
   return (
-    // <nav>
-    //   <div>
-    //     <h1>
-    //       <NavLink to='/'>Muroom</NavLink>
-    //     </h1>
-    //   </div>
-    //   <div>
-    //     <NavLink to='/places' exact='true'>
-    //       ALL PLACES
-    //     </NavLink>
-    //   </div>
-    //   <div>
-    //     <NavLink to='/map' exact='true'>
-    //       SHOW MAP
-    //     </NavLink>
-    //   </div>
-    // </nav>
-
     <div className='navigation'>
       <Link className='logo-container' to='/'>
         Muroom
       </Link>
       <div className='nav-links-container'>
         <Link className='nav-link' to='/places'>
-          ALL PLACES
+          PLACES
         </Link>
         <Link className='nav-link' to='/map'>
-          SHOW MAP
+          MAP
         </Link>
+        <form className='search-box-container'>
+          <input type='text' />
+          <button>SEARCH</button>
+        </form>
       </div>
     </div>
   );
